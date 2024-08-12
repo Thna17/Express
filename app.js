1894b73d8291662cac120c2728552531a3431b28
Hong Than Brathna <anbschool0021@AnBs-21-Mac-mini.local>
2024-08-12 19:00:38 +0700

    Mount the routers on specific routes

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const fs = require('fs')
 const express = require('express');
+const morgan = require('morgan');
+
 
 const app = express();
 
-// Middleware to log requests
+
+//  1. Middleware
+app.use(morgan('dev'))
 app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log('Hello from the middleware!');
+    next();
+})
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+})
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
+
+//  2. Routes handler
 const getAllTours = (req, res) => {
+    console.log(req.requestTime);
     res.status(200).json({
         status: 'success',
+        requestTime: req.requestTime,
         result: tours.length, 
         data: {
             tours,
@@ -80,6 +98,37 @@ const deleteTour =  (req, res) => {
     })
 }
 
+const getAllUsers = (req, res) => {
+    res.status(500).json( {
+        status: 'error',
+        message: 'Server Error'
+    })
+}
+
+const createUser = (req, res) => {
+    res.status(500).json( {
+        status: 'error',
+        message: 'Server Error'
+    })
+}
+const getUserById = (req, res) => {
+    res.status(500).json( {
+        status: 'error',
+        message: 'Server Error'
+    })
+}
+const updateUser = (req, res) => {
+    res.status(500).json( {
+        status: 'error',
+        message: 'Server Error'
+    })
+}
+const deleteUser = (req, res) => {
+    res.status(500).json( {
+        status: 'error',
+        message: 'Server Error'
+    })
+}
 
 // app.get('/api/v1/tours', getAllTours)
 // app.get('/api/v1/tours/:id', getTourById)
@@ -87,17 +136,33 @@ const deleteTour =  (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
-app
-    .route('/api/v1/tours')
-    .get(getAllTours)
-    .post(createTour);
+// 3. Route
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app.route('/api/v1/tours/:id')
-    .get(getTourById)
-    .patch(updateTour)
-    .delete(deleteTour);
+tourRouter
+.route('/')
+.get(getAllTours)
+.post(createTour)
 
+tourRouter
+.route('/:id')
+.get(getTourById)
+.patch(updateTour)
+.delete(deleteTour);
+userRouter.route('/')
+.get(getAllUsers)
+.post(createUser);
+userRouter.route('/:id')
+.get(getUserById)
+.patch(updateUser)
+.delete(deleteUser);
 
+// Mount the routers on specific routes
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+// 4. Start Server
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
