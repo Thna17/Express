66c409b362bd6eb7b00e0e6fbe430002e3425b99
Hong Than Brathna <anbschool0021@AnBs-21-Mac-mini.local>
2024-08-28 14:47:26 +0700

    update and delete controller

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,5 +1,4 @@
-const fs = require('fs');
-const Tour = require('./../models/tourModel');
+const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
@@ -19,19 +18,24 @@ exports.getAllTours = async (req, res) => {
   }
 };
 
-// exports.getTourById = (req, res) => {
-//   console.log(req.params);
-//   const id = req.params.id * 1;
-
-//   const tour = tours.find((el) => el.id === id);
-
-//   res.status(200).json({
-//     status: 'success',
-//     data: {
-//       tour,
-//     },
-//   });
-// };
+exports.getTourById = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    console.log(tour);
+    // Tour.findOne({_id: req.params.id})
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'No tour found with that ID',
+    });
+  }
+};
 
 exports.createTour = async (req, res) => {
   // const newTour = new Tour({})
@@ -52,18 +56,37 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'No tour found with that ID',
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'No tour found with that ID',
+    });
+  }
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const tourController = require('./../controller/tourController');
+const tourController = require('../controller/tourController');
 
-const {getAllTours, createTour, updateTour, deleteTour } = tourController;
+const { getAllTours, createTour, updateTour, deleteTour, getTourById } =
+  tourController;
 const router = express.Router();
 
 router.route('/').get(getAllTours).post(createTour);
 
-router.route('/:id').patch(updateTour).delete(deleteTour);
+router.route('/:id').get(getTourById).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
