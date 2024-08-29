80a76173c1da647f1c0be9bc61c77fa2b64968a7
Hong Than Brathna <anbschool0021@AnBs-21-Mac-mini.local>
2024-08-29 15:11:32 +0700

    limiting fields

--- controller/tourController.js
+++ controller/tourController.js
@@ -17,13 +17,22 @@ exports.getAllTours = async (req, res) => {
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
     let query = Tour.find(JSON.parse(queryStr));
-    
+
+    // 3. SORTING
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       console.log(sortBy);
       query = query.sort(sortBy);
     } else {
       query = query.sort('-createdAt');
+    }
+
+    // 4. Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
     }
     const tours = await query;
 
