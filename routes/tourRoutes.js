7a0fce975c05443188f9fd56aa7e6b315047c111
Hong Than Brathna <anbschool0021@AnBs-21-Mac-mini.local>
2024-08-29 16:05:50 +0700

    Aliasing

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const tourController = require('../controller/tourController');
 
-const { getAllTours, createTour, updateTour, deleteTour, getTourById } =
-  tourController;
+const {
+  aliasTopTours,
+  getAllTours,
+  createTour,
+  updateTour,
+  deleteTour,
+  getTourById,
+} = tourController;
 const router = express.Router();
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/').get(getAllTours).post(createTour);
 
